04f56a0fa66465ffb2d0879314b762eeeb9b8405
Igor Albanezi Conceição <igoralbanezi@globo.com>
2022-08-17 01:40:23 +0000

    Initialize project

---
+++ src/main/java/com/marketgateway/MarketGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marketgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketGatewayApplication.class, args);
+	}
+
+}
